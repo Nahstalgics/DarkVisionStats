f1c9cf7b8c445d71522924dd1e2462ad973a4bc9
Nahstalgic <colinyang593@gmail.com>
2026-06-23 10:18:52 -0700

    Initialized Repository

---
+++ main.py
@@ -0,0 +1,39 @@
+import numpy as np
+import scipy as sp
+import pandas as pd
+import statsmodels as sm
+import random
+from PyQt6 import QtWidgets, QtCore
+from PyQt6.QtWidgets import QApplication, QWidget
+import sys
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.text = QtWidgets.QLabel("Hello World",
+                                     alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+
+        self.setStyleSheet("")
+
+        self.button.clicked.connect(self.magic)
+
+    @QtCore.pyqtSlot()
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
